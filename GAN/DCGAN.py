7b864828f6f883222726973a96214be798366e3b
Daigo HIROOKA <tappy.hello-planet@ezweb.ne.jp>
2017-04-19 22:54:07 +0900

    confirm RGB image output

--- GAN/DCGAN.py
+++ GAN/DCGAN.py
@@ -81,13 +81,15 @@ def combine_images(generated_images):
     cols = int(math.sqrt(total))
     rows = math.ceil(float(total)/cols)
     width, height = generated_images.shape[1:3]
-    combined_image = np.zeros((height*rows, width*cols),
+    combined_image = np.zeros((height*rows, width*cols, 3),
                               dtype=generated_images.dtype)
     
     for index, image in enumerate(generated_images):
         i = int(index/cols)
         j = index % cols
-        combined_image[width*i:width*(i+1), height*j:height*(j+1)] = image[:, :, 0]
+        combined_image[width*i:width*(i+1), height*j:height*(j+1), :] \
+            = image
+        
     return combined_image
 
 
